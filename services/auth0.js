722b84bde55a36fe412ff7002cbd8bfad5cea67f
Yohan <yohan@yohanpro.com>
2020-02-06 20:26:28 +0900

    101번 redirect url 구현

--- services/auth0.js
+++ services/auth0.js
@@ -10,7 +10,27 @@ class Auth0 {
             scope: 'openid profile'
         });
         this.login = this.login.bind(this);
+        this.handleAuthentication = this.handleAuthentication.bind(this);
     }
+    handleAuthentication() {
+        return new Promise((resolve, reject) => {
+            this.auth0.parseHash((err, authResult) => {
+                // debugger;
+                if (authResult && authResult.accessToken && authResult.idToken) {
+                    this.setSession(authResult);
+                    resolve();
+                } else if (err) {
+                    reject(err);
+                    console.log(err);
+                }
+            });
+        });
+    }
+
+    setSession() {
+        //savetoken
+    }
+
     login() {
         this.auth0.authorize();
     }
