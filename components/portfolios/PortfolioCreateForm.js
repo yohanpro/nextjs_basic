42ff452d8bfa6524304bb49db0786249a4e37f2b
Yohan <yohan@yohanpro.com>
2020-02-25 23:17:31 +0900

    formik 라이브러리 추가

--- components/portfolios/PortfolioCreateForm.js
+++ components/portfolios/PortfolioCreateForm.js
@@ -1,49 +1,43 @@
+// Render Prop
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
+const PortfolioCreateForm = () => (
+    <div>
+        <h1>Any place in your app!</h1>
+        <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+                const errors = {};
+                if (!values.email) {
+                    errors.email = 'Required';
+                } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                    errors.email = 'Invalid email address';
+                }
+                return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            {({ isSubmitting }) => (
+                <Form>
+                    <Field type="email" name="email" />
+                    <ErrorMessage name="email" component="div" />
+                    <Field type="password" name="password" />
+                    <ErrorMessage name="password" component="div" />
+                    <button type="submit" disabled={isSubmitting}>
+                        Submit
+          </button>
+                </Form>
+            )}
+        </Formik>
+    </div>
+);
 
-class PortFolioCreateFrom extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { title: '', description: '', language: 'coconut' };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-
-    }
-
-    handleChange(event) {
-        const field = event.target.name;
-        this.setState({ [field]: event.target.value });
-    }
-
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.title);
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                <input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-
-                <label>
-                    Description:
-                 <textarea name="description" value={this.state.description} onChange={this.handleChange} />
-                </label>
-                <select name="language" value={this.state.select} onChange={this.handleChange}>
-                    <option value="grapefruit">Grapefruit</option>
-                    <option value="lime">Lime</option>
-                    <option value="coconut">Coconut</option>
-                    <option value="mango">Mango</option>
-                </select>
-                <input n type="submit" value="Submit" />
-            </form >
-        );
-    }
-}
-
-
-export default PortFolioCreateFrom;
+export default PortfolioCreateForm;
