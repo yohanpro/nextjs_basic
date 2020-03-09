41536c101dadf7e6cc7e73e3b51f378bd1c5f80b
Yohan <yohan@yohanpro.com>
2020-03-09 23:09:18 +0900

    slate editor에 Event handler 적용

--- components/slate-editor/Editor.js
+++ components/slate-editor/Editor.js
@@ -18,7 +18,16 @@ const SlateEditor = () => {
     // Render the Slate context.
     return (
         <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-            <Editable />
+            <Editable
+                onKeyDown={event => {
+                    if (event.key === '&') {
+                        // Prevent the ampersand character from being inserted.
+                        event.preventDefault();
+                        // Execute the `insertText` method when the event occurs.
+                        editor.insertText("and");
+                    }
+                }}
+            />
         </Slate>
     );
 };
