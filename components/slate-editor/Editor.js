9d618af52387a869002778b4e1cbc8a84b701ebd
Yohan <yohan@yohanpro.com>
2020-03-10 23:32:53 +0900

    slate editor에 메모이제이션 추가 custom Block slate

--- components/slate-editor/Editor.js
+++ components/slate-editor/Editor.js
@@ -1,10 +1,24 @@
 // Import React dependencies.
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState, useCallback } from "react";
 // Import the Slate editor factory.
 import { createEditor } from 'slate';
-
+import { Transforms, Editor } from 'slate';
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from 'slate-react';
+
+// Define a React component renderer for our code blocks.
+const CodeElement = props => {
+    return (
+        <pre {...props.attributes}>
+            <code>{props.children}</code>
+        </pre>
+    );
+};
+const DefaultElement = props => {
+    return <p {...props.attributes}>{props.children}</p>;
+};
+
+
 
 const SlateEditor = () => {
     const editor = useMemo(() => withReact(createEditor()), []);
@@ -14,17 +28,29 @@ const SlateEditor = () => {
             children: [{ text: 'A line of text in a paragraph.' }],
         },
     ]);
-
+    const renderElement = useCallback(props => {
+        switch (props.element.type) {
+            case 'code':
+                return <CodeElement {...props} />;
+            default:
+                return <DefaultElement {...props} />;
+        }
+    }, []);
     // Render the Slate context.
     return (
         <Slate editor={editor} value={value} onChange={value => setValue(value)}>
             <Editable
+                renderElement={renderElement}
                 onKeyDown={event => {
-                    if (event.key === '&') {
-                        // Prevent the ampersand character from being inserted.
+                    if (event.key === '`' && event.ctrlKey) {
+                        // Prevent the "`" from being inserted by default.
                         event.preventDefault();
-                        // Execute the `insertText` method when the event occurs.
-                        editor.insertText("and");
+                        // Otherwise, set the currently selected blocks type to "code".
+                        Transforms.setNodes(
+                            editor,
+                            { type: 'code' },
+                            { match: n => Editor.isBlock(editor, n) }
+                        );
                     }
                 }}
             />
