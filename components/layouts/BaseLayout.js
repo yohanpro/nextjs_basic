dd29c8b86047fa5cdd4c83f1fc193342a6679701
Yohan <yohan@yohanpro.com>
2020-04-12 19:27:19 +0900

    Robots.txt, favicon 추가

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,36 +1,69 @@
-import React from 'react';
-import Header from '../shared/Header';
-import Head from 'next/head';
+import React from "react";
+import Header from "../shared/Header";
+import Head from "next/head";
 
 const BaseLayout = (props) => {
-  const { className, children, isAuthenticated, user, isSiteOwner, cannonical } = props;
-  const headerType = props.headerType || 'default';
-  const title = props.title || 'Yohan Kim Portfolio';
+  const {
+    className,
+    children,
+    isAuthenticated,
+    user,
+    isSiteOwner,
+    cannonical,
+  } = props;
+  const headerType = props.headerType || "default";
+  const title = props.title || "Yohan Kim Portfolio";
   return (
     <React.Fragment>
       <Head>
         <title>{title}</title>
-        <meta name="description" content="My name is Yohan Kim and I am an experienced software engineer and freelance developer. I have a Master's degree in Artificial Intelligence and several years of experience working on a wide range of technologies and projects from C++ development for ultrasound devices to modern mobile and web applications in React and Angular. Throughout my career, I have acquired advanced technical knowledge and the ability to explain programming topics clearly and in detail to a broad audience. I invite you to take my course, where I have put a lot of effort to explain web and software engineering concepts in a detailed, hands-on and understandable way." />
-        <meta name="keywords" content="jerga portfolio, jerga developer, jerga freelancig, jerga programming" />
-        <meta property="og:title" content="Yohan Kim - programmer, developer, bloger" />
-        <meta property="og:locale" content="en_EU" />
+        <meta
+          name="description"
+          content="My name is Yohan Kim and I am an experienced software engineer and freelance developer. I have a Master's degree in Artificial Intelligence and several years of experience working on a wide range of technologies and projects from C++ development for ultrasound devices to modern mobile and web applications in React and Angular. Throughout my career, I have acquired advanced technical knowledge and the ability to explain programming topics clearly and in detail to a broad audience. I invite you to take my course, where I have put a lot of effort to explain web and software engineering concepts in a detailed, hands-on and understandable way."
+        />
+        <meta
+          name="keywords"
+          content="jerga portfolio, jerga developer, jerga freelancig, jerga programming"
+        />
+        <meta
+          property="og:title"
+          content="Yohan Kim - programmer, developer, bloger"
+        />
+        <meta property="og:locale" content="ko-KR" />
         <meta property="og:url" content={`${process.env.BASE_URL}`} />
         <meta property="og:type" content="website" />
-        <meta property="og:description" content="My name is Yohan Kim and I am an experienced software engineer and freelance developer." />
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" integrity="sha384-v8BU367qNbs/aIZIxuivaU55N5GPF89WBerHoGA4QTcbUjYiLQtKdrfXnqAcXyTv" crossOrigin="anonymous" />>
-        {cannonical && <link rel="cannonical" href={`${process.env.BASE_URL}${cannonical}`} />}
+        <meta
+          property="og:description"
+          content="My name is Yohan Kim and I am an experienced software engineer and freelance developer."
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat:400,700"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.12.1/css/all.css"
+          integrity="sha384-v8BU367qNbs/aIZIxuivaU55N5GPF89WBerHoGA4QTcbUjYiLQtKdrfXnqAcXyTv"
+          crossOrigin="anonymous"
+        />
+        >
+        {cannonical && (
+          <link
+            rel="cannonical"
+            href={`${process.env.BASE_URL}${cannonical}`}
+          />
+        )}
         <link rel="icon" type="image/ico" href="/static/favicon.ico" />
       </Head>
-      <div className="layout-container" >
-        <Header className={`port-nav-${headerType}`}
+      <div className="layout-container">
+        <Header
+          className={`port-nav-${headerType}`}
           isAuthenticated={isAuthenticated}
           user={user}
-          isSiteOwner={isSiteOwner} />
+          isSiteOwner={isSiteOwner}
+        />
         <main className={`cover ${className}`}>
-          <div className="wrapper">
-            {children}
-          </div>
+          <div className="wrapper">{children}</div>
         </main>
       </div>
     </React.Fragment>
@@ -38,4 +71,3 @@ const BaseLayout = (props) => {
 };
 
 export default BaseLayout;
-
--- server/index.js
+++ server/index.js
@@ -1,76 +1,90 @@
-const express = require('express');
-const next = require('next');
-const routes = require('../routes.js');
-const mongoose = require('mongoose');
+const express = require("express");
+const next = require("next");
+const routes = require("../routes.js");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handle = routes.getRequestHandler(app);
-const authServices = require('./services/auth');
-const config = require('./config');
+const authServices = require("./services/auth");
+const config = require("./config");
 
-const bookRouter = require('./routes/book');
-const portfolioRouter = require('./routes/portfolio');
-const blogRouter = require('./routes/blog');
+const bookRouter = require("./routes/book");
+const portfolioRouter = require("./routes/portfolio");
+const blogRouter = require("./routes/blog");
+
 // With express
 
 const secretData = [
-    {
-        title: '안녕',
-        description: "저리가라 임마"
-    },
-    {
-        title: "그래 안녕?",
-        description: "너나 저리가라"
-
-    }
+  {
+    title: "안녕",
+    description: "저리가라 임마",
+  },
+  {
+    title: "그래 안녕?",
+    description: "너나 저리가라",
+  },
 ];
 
-mongoose.connect(config.DB_URI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-)
-    .then(() => {
-        console.log('Database Connected');
-    })
-    .catch(err => console.log(err));
+const robotsOptions = {
+  root: path.join(__dirname, "../static"),
+  headers: {
+    "Content-Type": "text/plain;charset=UTF-8",
+  },
+};
+mongoose
+  .connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => console.log(err));
 
-app.prepare()
-    .then(() => {
-        const server = express();
-        server.use(express.json());
-        server.use(express.urlencoded({ extended: true }));
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
 
-        server.use('/api/v1/books', bookRouter);
-        server.use('/api/v1/portfolios', portfolioRouter);
-        server.use('/api/v1/blogs', blogRouter);
+    server.use("/api/v1/books", bookRouter);
+    server.use("/api/v1/portfolios", portfolioRouter);
+    server.use("/api/v1/blogs", blogRouter);
 
-
-        server.get('/api/v1/secret', authServices.checkJWT, (req, res) => {
-            return res.json(secretData);
-        });
-        server.get('/api/v1/onlysiteowner', authServices.checkJWT, authServices.checkRole("siteOwner"), (req, res) => {
-            return res.json(secretData);
-        });
-        server.get('*', (req, res) => {
-            return handle(req, res);
-        });
-
-        server.use(function (err, req, res, next) {
-            console.log('err', err);
-            if (err.name === 'UnauthorizedError') {
-                res.status(401).send('invalid token...');
-            }
-        });
-
-        server.use(handle).listen(3000, err => {
-            if (err) throw err;
-            console.log('Server is no on 3000');
-        });
-    })
-    .catch(ex => {
-        console.error(ex.stack);
-        process.exit(1);
+    server.get("/robots.txt", (req, res) => {
+      return res.status(200).sendFile("robots.txt", robotsOptions);
     });
 
+    server.get("/api/v1/secret", authServices.checkJWT, (req, res) => {
+      return res.json(secretData);
+    });
+    server.get(
+      "/api/v1/onlysiteowner",
+      authServices.checkJWT,
+      authServices.checkRole("siteOwner"),
+      (req, res) => {
+        return res.json(secretData);
+      }
+    );
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.use(function (err, req, res, next) {
+      console.log("err", err);
+      if (err.name === "UnauthorizedError") {
+        res.status(401).send("invalid token...");
+      }
+    });
+
+    server.use(handle).listen(3000, (err) => {
+      if (err) throw err;
+      console.log("Server is no on 3000");
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
