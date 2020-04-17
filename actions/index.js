6d92ed10f0797506cc940be0fcbadf7f38f6f548
Yohan <yohan@yohanpro.com>
2020-04-17 21:31:38 +0900

    action.js 오류 수정

--- actions/index.js
+++ actions/index.js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 import { getCookieFromReq } from "../helpers/utils";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.BASE_URL}/api/v1/`,
+  baseURL: `${process.env.BASE_URL}/api/v1`,
   timeout: 3000,
 });
 
