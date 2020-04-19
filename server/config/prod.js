dad8a5b82897a4634007c3b6d1c5384efa0e38f4
Yohan <yohan@yohanpro.com>
2020-04-19 23:35:04 +0900

    mongodb uri 수정

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,7 @@
 module.exports = {
   //   DB_URI:
   //     "mongodb+srv://yohan:yohanyohan@cluster0-tym4s.mongodb.net/test?retryWrites=true&w=majority",
-  DB_URI: process.env.DB_URI,
+  DB_URI:
+    "mongodb+srv://yohanKim:yohanKim@cluster0-kpmtn.mongodb.net/test?retryWrites=true&w=majority",
   NAMESPACE: "https://portfolio-yohan.herokuapp.com",
 };
