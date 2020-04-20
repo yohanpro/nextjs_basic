76ce23aefccea55dc63d739c343541a8a60fa409
Yohan <yohan@yohanpro.com>
2020-04-20 15:32:01 +0900

    mongodbURI 수정

--- env-config.js
+++ env-config.js
@@ -6,6 +6,7 @@ module.exports = {
     : "http://localhost:3000",
   "process.env.NAMESPACE": "https://portfolio-yohan.herokuapp.com",
   "process.env.CLIENT_ID": "akEPVW71HpnNNu4hlbJ6h6BDd4YOVvO0",
-  "process.env.DB_URI":
-    "mongodb+srv://yohanyohan:yohanyohan@cluster0-kpmtn.mongodb.net/test?retryWrites=true&w=majority",
+  "process.env.DB_URI": prod
+    ? "mongodb+srv://yohanKim:yohanKim@cluster0-kpmtn.mongodb.net/test?retryWrites=true&w=majority"
+    : "mongodb+srv://yohan:yohan@cluster0-tym4s.mongodb.net/test?retryWrites=true&w=majority",
 };
--- server/config/prod.js
+++ server/config/prod.js
@@ -2,7 +2,6 @@ module.exports = {
   //   DB_URI:
   //     "mongodb+srv://yohan:yohanyohan@cluster0-tym4s.mongodb.net/test?retryWrites=true&w=majority",
   // DB_URI: process.env.DB_URI,
-  DB_URI:
-    "mongodb+srv://yohanyohan:yohanyohan@cluster0-kpmtn.mongodb.net/test?retryWrites=true&w=majorit",
+  DB_URI: process.env.DB_URI,
   NAMESPACE: "https://portfolio-yohan.herokuapp.com",
 };
