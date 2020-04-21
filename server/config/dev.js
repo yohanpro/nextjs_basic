2f7ee92ed53eb9f214ad239519b90f24b82bde17
Yohan <yohan@yohanpro.com>
2020-04-21 22:23:49 +0900

    heroku deploy용 setting

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // DB_URI: 'mongodb+srv://yohan:Mr9KeNT7TQyNFlkH@cluster0-tym4s.mongodb.net/test?retryWrites=true&w=majority'
-  DB_URI:
-    "mongodb+srv://yohan:yohan@cluster0-tym4s.mongodb.net/test?retryWrites=true&w=majority",
-};
--- server/config/prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  DB_URI:
-    "mongodb+srv://yohanKim:yohanKimPro@cluster0-kpmtn.mongodb.net/test?retryWrites=true&w=majority",
-  NAMESPACE: "https://portfolio-yohan.herokuapp.com",
-};
